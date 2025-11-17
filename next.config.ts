5427cda3145ba3037bfc886e9a47a43823a7f833
devjiwonchoi <devjiwonchoi@gmail.com>
2025-11-17 12:37:28 +0100

    Remove withSentryConfig

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
-import { withSentryConfig } from '@sentry/nextjs'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {}
 
-export default withSentryConfig(nextConfig)
+export default nextConfig
